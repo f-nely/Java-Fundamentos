274a14b15edd363f099eeb74ff0c9fc6d39dcc69
nely de faria <nelly.nf54@gmail.com>
2020-06-28 19:11:38 -0300

    Understanding the difference between next () and nextLine ()

--- Java-Fundamentos/src/com/nelynely/Main.java
+++ Java-Fundamentos/src/com/nelynely/Main.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Type your name: ");
-        String name = scan.nextLine();
-        System.out.println("Hey " + name);
+        System.out.println("Enter the first number: ");
+        int number1 = scan.nextInt();
+        System.out.println("Inform the operation: ");
+        char operation = scan.next().charAt(0);
+        System.out.println("Enter the second number: ");
+        int number2 = scan.nextInt();
         scan.close();
     }
 }

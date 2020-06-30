712af1fbb7166f3189555053a6647f7e3ebc4fc2
nely de faria <nelly.nf54@gmail.com>
2020-06-30 18:47:35 -0300

    Learning the while repetition structure

--- Java-Fundamentos/src/com/nelynely/Main.java
+++ Java-Fundamentos/src/com/nelynely/Main.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
-        int number1 = scan.nextInt();
-        System.out.println("Inform the operation: ");
-        char operation = scan.next().charAt(0);
-        System.out.println("Enter the second number: ");
-        int number2 = scan.nextInt();
-        System.out.println(String.format("Do you want to do the operation %d %c %d ?", number1, operation, number2));
-        int resul = 0;
+        int action = 1; // 0 = exit; 1 = mathematical operation: 2 = historic
+        ArrayList<String> historic = new ArrayList<String>();
+        while (action > 0) {
+            if (action == 1) {
+                System.out.println("Enter the first number: ");
+                int number1 = scan.nextInt();
+                System.out.println("Inform the operation: ");
+                char operation = scan.next().charAt(0);
+                System.out.println("Enter the second number: ");
+                int number2 = scan.nextInt();
+                System.out.println(String.format("Do you want to do the operation %d %c %d ?", number1, operation, number2));
+                int resul = 0;
         /*if (operation == '+'){
             resul = number1 + number2;
         } else if (operation == '-'){
@@ -25,27 +29,32 @@ public class Main {
         } else {
             System.out.println("Invalid operation: ");
         }*/
-        switch (operation){
-            case '+':
-                resul = number1 + number2;
-                break;
-            case '-':
-                resul = number1 - number2;
-                break;
-            case '*':
-                resul = number1 * number2;
-                break;
-            case '/':
-                resul = number1 / number2;
-                break;
-            default:
-                System.out.println("Invalid operation: ");
-                break;
+                switch (operation) {
+                    case '+':
+                        resul = number1 + number2;
+                        break;
+                    case '-':
+                        resul = number1 - number2;
+                        break;
+                    case '*':
+                        resul = number1 * number2;
+                        break;
+                    case '/':
+                        resul = number1 / number2;
+                        break;
+                    default:
+                        System.out.println("Invalid operation: ");
+                        break;
+                }
+
+                String inputHistoric = String.format("%d %c %d = %d ", number1, operation, number2, resul);
+                historic.add(inputHistoric);
+                System.out.println();
+            } else if (action == 2) {
+                System.out.println("----Historic----");
+                System.out.println(String.format("You already did %d operation ", historic.size()));
+            }
         }
-        ArrayList<String> historic = new ArrayList<String>();
-        String inputHistoric = String.format("%d %c %d = %d ", number1, operation, number2, resul);
-        historic.add(inputHistoric);
-        System.out.println();
         scan.close();
     }
 }
